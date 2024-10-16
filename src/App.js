8f479ff5dccbe843439c7755ccf2a34d8dd81e25
Megha Kale <mithunkalem2@gmail.com>
2024-10-17 00:04:56 +0530

    admin code completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Navbar from './componants/Navbar/Navbar';
+import Sidebar from './componants/Sidebar/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add/Add';
+import List from './pages/List/List';
+import Orders from './pages/Orders/Orders';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
+
+  const url = "http://localhost:4000";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className='app-content'>
+        <Sidebar />
+        <Routes>
+          <Route path='/add' element={<Add url={url} />} />
+          <Route path='/list' element={<List url={url} />} />
+          <Route path='/orders' element={<Orders url={url} />} />
+        </Routes>
+      </div>
     </div>
   );
 }
